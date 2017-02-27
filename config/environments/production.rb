f12e385f194a93a7d853154e6cdbe6c93d29d165
Sang Jun Park <sangjunpark@dyn-160-39-143-137.dyn.columbia.edu>
2017-02-26 21:18:46 -0500

    user log in and database updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Force all access to the app over SSL and use secure cookies
+  config.forces_ssl = true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/activities', to: 'static_pages#activities'
-  get 'users/new'
-#  get 'static_pages/home'
-#  get 'static_pages/help'
-#  get 'static_pages/about'
-#  get 'static_pages/search'
-#  root 'application#hello'
+  get '/signup', to: 'users#new'
+  
+  resources :users
 end
